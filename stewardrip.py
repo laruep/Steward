5204ff68ae9c5bdee0bf1ebb21e36921524a75b9
Paul LaRue <laruep@rpi.edu>
2018-03-19 13:17:19 -0400

    changed from csv module and did from scratch

--- stewardrip.py
+++ stewardrip.py
@@ -12,15 +12,27 @@ chat = sys.argv[1]
 # lists
 date = []
 name = []
-request = []
-favs = []
+reqpairs = []
 
 
 if __name__ == "__main__":
-	with open(chat, 'rb') as csvfile:
-		spamreader = csv.reader(csvfile,delimiter=',', quotechar='|')
-		for row in spamreader:
-			print row[3]
+	with open(chat) as f:
+		content = f.readlines()
+
+# Find third comma and split string
+reqp = content[0].split(',',3)
+reqp.pop(0)
+reqp.pop(0)
+reqp.pop(0)
+
+reqt = (reqp[0].rsplit(',',1))[0]
+reqf = (reqp[0].rsplit(',',1))[1]
+print reqt
+print reqf
+
+reqpairs.append(())
+
+#print req
 
 
 
